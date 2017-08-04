1e3479a679e6c9a0ba6b9ffbce0c6dca941f4636
Sirfame Lin <sifame@hotmail.com>
2017-08-03 19:41:39 -0700

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,372 @@
+'use-strict'
+var Alexa = require('alexa-sdk');
+var http = require('http');
+
+var states = {
+    SEARCHMODE: '_SEARCHMODE',
+    TOPFIVE: '_TOPFIVE',
+};
+
+var location = "Bellevue";
+
+var numberOfResults = 3;
+
+var APIKey = "f13040651817418581791f5e28921cb4";
+
+var welcomeMessage = location + " Guide. You can ask me for an attraction, the local news, or  say help. What will it be?";
+
+var welcomeRepromt = "You can ask me for an attraction, the local news, or say help. What will it be?";
+
+var locationOverview = "Bellevue is a city in the Eastside region of King County, Washington, United States, across Lake Washington from Seattle. As the third-largest city in the Seattle metropolitan area, Bellevue has variously been characterized as an edge city, a boomburb, or satellite city. The city had a population of 122,363 at the 2010 census.";
+
+var HelpMessage = "Here are some things you  can say: Give me an attraction. Tell me about " + location + ". Tell me the top five things to do. Tell me the local news.  What would you like to do?";
+
+var moreInformation = "See your  Alexa app for  more  information."
+
+var tryAgainMessage = "please try again."
+
+var noAttractionErrorMessage = "What attraction was that? " + tryAgainMessage;
+
+var topFiveMoreInfo = " You can tell me a number for more information. For example, open number one.";
+
+var getMoreInfoRepromtMessage = "What number attraction would you like to hear about?";
+
+var getMoreInfoMessage = "OK, " + getMoreInfoRepromtMessage;
+
+var goodbyeMessage = "OK, have a nice time in " + location + ".";
+
+var newsIntroMessage = "These are the " + numberOfResults + " most recent " + location + " headlines, you can read more on your Alexa app. ";
+
+var hearMoreMessage = "Would you like to hear about another top thing that you can do in " + location +"?";
+
+var newline = "\n";
+
+var output = "";
+
+var alexa;
+
+var attractions = [
+    {
+        name: "Bellevue Square.",
+        content: "Experience excellency in Bellevue's finest shopping center: indulge in shopping, dining, and entertainment.",
+        location: "575 Bellevue Square, Bellevue, Washington 98004.",
+        contact: "425 646 3660"
+    },
+    {
+        name: "Bellevue Downtown Park.",
+        content: "Expansive, 20 acre park situated in the heart of Downtown Bellevue.",
+        location: "10201 North east 4th Street, Bellevue, Washington 98004.",
+        contact: "425 452 6885"
+    },
+    {
+        name: "Meydenbauer Center.",
+        content: "Located next to Interstate 405, Meydenbauer Center provides Bellevue with a 36,000 square foot, fully carpeted, newly renovated Exhibition Hall.",
+        location: "11100 North east 6th Street, Bellevue, Washington 98004.",
+        contact: "425 637 1020"
+    },
+    {
+        name: "Bellevue Botanical Garden.",
+        content: "Free admission to Bellevue's Botanical Garden. Explore displays of garden, woodlands, meadows and wetlands.",
+        location: "12001 Main Street, Bellevue, Washington 98005.",
+        contact: "425 452 2750"
+    },
+    {
+        name: "Din Tai Fung Restaurant.",
+        content: "The absolute best Dim Sum experience in Bellevue. Originally from the country of Taiwan, Din Tai Fung offers royal service with traditional Chinese cuisine.",
+        location: "700 Bellevue Way North east Suite 280, Bellevue, Washington 98004.",
+        contact: "425 698 1095."
+    }
+];
+
+var topFive = [
+    {
+        number: "1",
+        caption: "Enjoy a high quality shopping and dining experience.",
+        more: "Bellevue Square provides a unique and unforgettable experience with over 150 retail stores, spacious atriums, and endless entertainment.",
+        location: "575 Bellevue Square, Bellevue, Washington 98004.",
+        contact: "425 646 3660"
+    },
+    {
+        number: "2",
+        caption: "Escape the busy city at Bellevue Park.",
+        more: "With the completion of the Complete the Circle project, come enjoy the park's 240 foot wide waterfall and reflection pond. The delightful area and gardens add for the perfect opportunity for picnics and gatherings.",
+        location: "10201 North east 4th street, Bellevue, Washington 98004.",
+        contact: "425 452 6885"
+    },
+    {
+        number: "3",
+        caption: "Attend a wide breath of events at the Exhibition Hall of Meydenbauer Center.",
+        more: "Enjoy over 250 convention events, including banquets, consumer shows, and community events. Meydenbaur Center is home to a diverse culture of performing arts and community.",
+        location: "11100 North East 6th Street, Bellevue, Washington 98004.",
+        contact: "425 637 1020"
+    },
+    {
+        number: "4",
+        caption: "Floral and Landscaping Extravagance.",
+        more: "Discover natural garden care practices, tiny flowers, rock gardens and a Wonderland of Nature. After a transcendental visit, shop at the Visitor Center, or take classes with the garden community!",
+        location: "12001 Main Street, Bellevue, Washington 98005.",
+        contact: "425 452 2750"
+    },
+    {
+        number: "5",
+        caption: "Superior Asian dining experience.",
+        more: "Internationally recognized xiaolongbao, the Din Tai Fung chain offers a mind blowing dining experience.",
+        location: "700 Bellevue Way North east suite 280, Bellevue, Washington 98004.",
+        contact: "425 698 1095."
+    }
+];
+
+
+var topFiveIntro = "Here are the top five things to do in Bellevue."; //var topFiveIntro = "Here are the top five things to do in " + location + ".";
+
+var newSessionHandlers = {
+    'LaunchRequest': function () {
+        this.handler.state = states.SEARCHMODE;
+        output = welcomeMessage;
+        this.emit(':ask', output, welcomeRepromt);
+    },
+    'getOverview': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getOverview');
+    },
+    'getNewsIntent': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getNewsIntent');
+    },
+    'getAttractionIntent': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getAttractionIntent');
+    },
+    'getTopFiveIntent': function(){
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getTopFiveIntent');
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.CancelIntent': function () {
+        // Use this function to clear up and save any data needed between sessions
+        this.emit(":tell", goodbyeMessage);
+    },
+    'SessionEndedRequest': function () {
+        // Use this function to clear up and save any data needed between sessions
+        this.emit('AMAZON.StopIntent');
+    },
+    'Unhandled': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, welcomeRepromt);
+    },
+};
+
+var startSearchHandlers = Alexa.CreateStateHandler(states.SEARCHMODE, {
+    'getOverview': function () {
+        output = locationOverview;
+        this.emit(':tellWithCard', output, location, locationOverview);
+    },
+    'getAttractionIntent': function () {
+        var cardTitle = location;
+        var cardContent = "";
+
+        var attraction = attractions[Math.floor(Math.random() * attractions.length)];
+        if (attraction) {
+            output = attraction.name + " " + attraction.content + newline + moreInformation;
+            cardTitle = attraction.name;
+            cardContent = attraction.content + newline + attraction.contact;
+
+            this.emit(':tellWithCard', output, cardTitle, cardContent);
+        } else {
+            this.emit(':ask', noAttractionErrorMessage, tryAgainMessage);
+        }
+    },
+    'getTopFiveIntent': function () {
+        output = topFiveIntro;
+        var cardTitle = "Top Five Things To See in " + location;
+
+        for (var counter = topFive.length - 1; counter >= 0; counter--) {
+            output += " Number " + topFive[counter].number + ": " + topFive[counter].caption + "," + newline;
+        }
+        output += topFiveMoreInfo;
+        this.handler.state = states.TOPFIVE;
+        this.emit(':askWithCard', output, topFiveMoreInfo, cardTitle, output);
+    },
+    'AMAZON.YesIntent': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, HelpMessage);
+    },
+    'AMAZON.NoIntent': function () {
+        output = HelpMessage;
+        this.emit(':ask', HelpMessage, HelpMessage);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.HelpIntent': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, HelpMessage);
+    },
+    'getNewsIntent': function () {
+        httpGet(location, function (response) {
+
+            // Parse the response into a JSON object ready to be formatted.
+            var responseData = JSON.parse(response);
+            var cardContent = "Data provided by New York Times\n\n";
+
+            // Check if we have correct data, If not create an error speech out to try again.
+            if (responseData == null) {
+                output = "There was a problem with getting data please try again";
+            }
+            else {
+                output = newsIntroMessage;
+
+                // If we have data.
+                for (var i = 0; i < responseData.response.docs.length; i++) {
+
+                    if (i < numberOfResults) {
+                        // Get the name and description JSON structure.
+                        var headline = responseData.response.docs[i].headline.main;
+                        var index = i + 1;
+
+                        output += " Headline " + index + ": " + headline + ";";
+
+                        cardContent += " Headline " + index + ".\n";
+                        cardContent += headline + ".\n\n";
+                    }
+                }
+
+                output += " See your Alexa app for more information.";
+            }
+
+            var cardTitle = location + " News";
+
+            alexa.emit(':tellWithCard', output, cardTitle, cardContent);
+        });
+    },
+
+    'AMAZON.RepeatIntent': function () {
+        this.emit(':ask', output, HelpMessage);
+    },
+    'AMAZON.CancelIntent': function () {
+        // Use this function to clear up and save any data needed between sessions
+        this.emit(":tell", goodbyeMessage);
+    },
+    'SessionEndedRequest': function () {
+        // Use this function to clear up and save any data needed between sessions
+        this.emit('AMAZON.StopIntent');
+    },
+    'Unhandled': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, welcomeRepromt);
+    }
+});
+
+var topFiveHandlers = Alexa.CreateStateHandler(states.TOPFIVE, {
+    'getAttractionIntent': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getAttractionIntent');
+    },
+    'getOverview': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getOverview');
+    },
+    'getTopFiveIntent': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getTopFiveIntent');
+    },
+    'AMAZON.HelpIntent': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, HelpMessage);
+    },
+
+    'getMoreInfoIntent': function () {
+        var slotValue = 0;
+        if(this.event.request.intent.slots.attraction ) {
+            if (this.event.request.intent.slots.attraction.value) {
+                slotValue = this.event.request.intent.slots.attraction.value;
+
+            }
+        }
+
+        if (slotValue > 0 && slotValue <= topFive.length) {
+
+            var index = parseInt(slotValue) - 1;
+            var selectedAttraction = topFive[index];
+
+            output = selectedAttraction.caption + ". " + selectedAttraction.more + ". " + hearMoreMessage;
+            var cardTitle = selectedAttraction.name;
+            var cardContent = selectedAttraction.caption + newline + newline + selectedAttraction.more + newline + newline + selectedAttraction.location + newline + newline + selectedAttraction.contact;
+
+            this.emit(':askWithCard', output, hearMoreMessage, cardTitle, cardContent);
+        } else {
+            this.emit(':ask', noAttractionErrorMessage);
+        }
+    },
+
+    'AMAZON.YesIntent': function () {
+        output = getMoreInfoMessage;
+        alexa.emit(':ask', output, getMoreInfoRepromtMessage);
+    },
+    'AMAZON.NoIntent': function () {
+        output = goodbyeMessage;
+        alexa.emit(':tell', output);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.RepeatIntent': function () {
+        this.emit(':ask', output, HelpMessage);
+    },
+    'AMAZON.CancelIntent': function () {
+        // Use this function to clear up and save any data needed between sessions
+        this.emit(":tell", goodbyeMessage);
+    },
+    'SessionEndedRequest': function () {
+        // Use this function to clear up and save any data needed between sessions
+    },
+
+    'Unhandled': function () {
+        output = HelpMessage;
+        this.emit(':ask', output, welcomeRepromt);
+    }
+});
+
+exports.handler = function (event, context, callback) {
+    alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(newSessionHandlers, startSearchHandlers, topFiveHandlers);
+    alexa.execute();
+};
+
+// Create a web request and handle the response.
+function httpGet(query, callback) {
+    console.log("/n QUERY: "+query);
+
+    var options = {
+        //http://api.nytimes.com/svc/search/v2/articlesearch.json?q=seattle&sort=newest&api-key=
+        host: 'api.nytimes.com',
+        path: '/svc/search/v2/articlesearch.json?q=' + query + '&sort=newest&api-key=' + APIKey,
+        method: 'GET'
+    };
+
+    var req = http.request(options, (res) => {
+
+            var body = '';
+
+    res.on('data', (d) => {
+        body += d;
+});
+
+    res.on('end', function () {
+        callback(body);
+    });
+
+});
+    req.end();
+
+    req.on('error', (e) => {
+        console.error(e);
+});
+}
+
+String.prototype.trunc =
+    function (n) {
+        return this.substr(0, n - 1) + (this.length > n ? '&hellip;' : '');
+    };
---
+++ temp.js
@@ -0,0 +1,70 @@
+var attractions = [
+    {
+        name: "Bellevue Square",
+        content: "Experience excellency in Bellevue's finest shopping center: indulge in shopping, dining, and entertainment.",
+        location: "575 Bellevue Square, Bellevue, Washington 98004",
+        contact: "425 646 3660"
+    },
+    {
+        name: "Bellevue Downtown Park",
+        content: "Expansive, 20 acre park situated in the heart of Downtown Bellevue",
+        location: "10201 North east 4th Street, Bellevue, Washington 98004",
+        contact: "425 452 6885"
+    },
+    {
+        name: "Meydenbauer Center",
+        content: "Located next to Interstate 405, Meydenbauer Center provides Bellevue with a 36,000 square foot, fully carpeted, newly renovated Exhibition Hall.",
+        location: "11100 North east 6th Street, Bellevue, Washington 98004",
+        contact: "425 637 1020"
+    },
+    {
+        name: "Bellevue Botanical Garden",
+        content: "Free admission to Bellevue's Botanical Garden. Explore displays of garden, woodlands, meadows and wetlands.",
+        location: "12001 Main Street, Bellevue, Washington 98005",
+        contact: "425 452 2750"
+    },
+    {
+        name: "Din Tai Fung Restaurant",
+        content: "The absolute best Dim Sum experience in Bellevue. Originally from the country of Taiwan, Din Tai Fung offers royal service with traditional Chinese cuisine.",
+        location: "700 Bellevue Way North east Suite 280, Bellevue, Washington 98004",
+        contact: "425 698 1095."
+    }
+];
+
+var topFive = [
+    {
+        number: "1",
+        caption: "Enjoy a high quality shopping and dining experience",
+        more: "Bellevue Square provides a unique and unforgettable experience with over 150 retail stores, spacious atriums, and endless entertainment",
+        location: "575 Bellevue Square, Bellevue, Washington 98004",
+        contact: "425 646 3660"
+    },
+    {
+        number: "2",
+        caption: "Escape the busy city at Bellevue Park",
+        more: "With the completion of the "Complete the Circle" project, come enjoy the park's 240 foot wide waterfall and reflection pond. The delightful area and gardens add for the perfect opportunity for picnics and gatherings.",
+        location: "10201 North east 4th street, Bellevue, Washington 98004",
+        contact: "425 452 6885"
+    },
+    {
+        number: "3",
+        caption: "Attend a wide breath of events at the Exhibition Hall of Meydenbauer Center",
+        more: "Enjoy over 250 convention events, including banquets, consumer shows, and community events. Meydenbaur Center is home to a diverse culture of performing arts and community.",
+        location: "11100 North East 6th Street, Bellevue, Washington 98004",
+        contact: "425 637 1020"
+    },
+    {
+        number: "4",
+        caption: "Floral and Landscaping Extravagance",
+        more: "Discover natural garden care practices, tiny flowers, rock gardens and a Wonderland of Nature. After a transcendental visit, shop at the Visitor Center, or take classes with the garden community!",
+        location: "12001 Main Street, Bellevue, Washington 98005",
+        contact: "425 452 2750"
+    },
+    {
+        number: "5",
+        caption: "Superior Asian dining experience".,
+        more: "Internationally recognized xiaolongbao, the Din Tai Fung chain offers a mind blowing dining experience",
+        location: "700 Bellevue Way North east suite 280, Bellevue, Washington 98004",
+        contact: "425 698 1095."
+    }
+];
